9ea855eb8eafddf34feedf7f71d8722ea9656f0a
Jauri da Cruz Junior <jauriads@gmail.com>
2021-04-17 20:42:21 -0300

    Primeiro projeto com Spring Batch

---
+++ src/main/java/jauri/junior/primeiroprojetospringbatch/BatchConfig.java
@@ -0,0 +1,41 @@
+package jauri.junior.primeiroprojetospringbatch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BatchConfig {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job batchProcessing() {
+
+		return jobBuilderFactory.get("batchProcessing").start(batchExecute()).build();
+
+	}
+
+	public Step batchExecute() {
+		return stepBuilderFactory.get("batchExecute").tasklet(new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("Hello World, Spring Batch...");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
+
+}
---
+++ src/test/java/jauri/junior/primeiroprojetospringbatch/PrimeiroProjetoSpringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package jauri.junior.primeiroprojetospringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
